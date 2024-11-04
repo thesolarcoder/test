009d2d4600113fbc977df9b615f4c6ba025953df
Abhinav Vishwakarma <inkedabhinav@icloud.com>
2024-11-04 17:45:16 +0530

    loaded custom font and exit key

--- main.go
+++ main.go
@@ -7,11 +7,24 @@ func main() {
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
+	var font_size int32 = 32
+	font := rl.LoadFontEx("./FiraSans-Regular.otf", font_size, nil)
+
+	rl.SetExitKey(rl.KeyQ)
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
-		len := rl.MeasureText("Welcome!", 50)
-		rl.DrawText("Welcome!", 800/2-len/2, 450/2-50/2, 50, rl.RayWhite)
+		dimen := rl.MeasureTextEx(font, "Welcome! Press q to exit", float32(font_size), 0)
+
+		pos := rl.Vector2{
+			X: 800/2 - dimen.X/2,
+			Y: 450/2 - dimen.Y/2,
+		}
+		if rl.IsKeyPressed(rl.KeyQ) {
+			rl.CloseWindow()
+		}
+		rl.DrawTextEx(font, "Welcome! Press q to exit", pos, float32(font_size), 0, rl.RayWhite)
 		rl.EndDrawing()
 	}
 
